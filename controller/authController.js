016cfca2ac006379b1e18d2be6c7322b7bb491f3
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2025-10-21 16:59:50 +0500

    finally push

--- controller/authController.js
+++ controller/authController.js
@@ -29,7 +29,7 @@ const login = async (req, res) => {
     // res
     res.cookie("token", token, {
         httpOnly: true,
-        secure: false,
+        secure: true,
         sameSite: "None",
         path: '/'
     }).json({ success: true, message: 'Login successful', user: _.pick(user, ['_id', 'name', 'email', 'role', 'wishlist', 'cart', 'order']) })
